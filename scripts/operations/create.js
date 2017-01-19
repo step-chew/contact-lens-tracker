e21034255eb7d78c09d2480a0f17e0fd25f053b7
Stephen Chew <github@step.chew.es>
2017-01-19 23:08:11 +0000

    pass in geolocation when creating an event

--- scripts/operations/create.js
+++ scripts/operations/create.js
@@ -3,9 +3,9 @@
 const gcal = require('../common/gcal');
 const ifttt = require('../common/ifttt')(process.env.IFTTT_MAKER);
 
-module.exports = (response) =>
+module.exports = (geolocation) =>
     gcal.authenticate()
         .then(resp =>
-            gcal.newEvent(resp.access_token, 'New Contact Lens')
+            gcal.newEvent(resp.access_token, 'New Contact Lens', geolocation)
         )
         .then(() => ifttt.notify('AWS', 'New Contact Lens'))
