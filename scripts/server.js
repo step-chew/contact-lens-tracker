aae1f665fba59da0cc91ffc22344f78bddf68ab4
Stephen <github@step.chew.es>
2017-10-15 01:11:26 +0800

    Added body-parser to parse request body. 

--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const contactLensRoute = require('./routes/contact-lens.route');
 
 const app = express();
 
 const port = parseInt(process.argv[2], 10) || 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/contact-lens', contactLensRoute);
 
 app.listen(port, () => {
